41bb087deddd487bc51cd96179332fc8a0390eff
Rennan Marcos Wanzo <145737494+RMWanzo@users.noreply.github.com>
2023-11-21 22:33:31 -0300

    Add files via upload

---
+++ a32023/src/main/java/com/usjt/a32023/A32023.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.usjt.a32023;
+
+import SQLConnect.ConnectMSQL;
+import TelaLogin.Login;
+
+/**
+ *
+ * @author RENNAN
+ */
+public class A32023 {
+
+    public static void main(String[] args)
+    {
+        ConnectMSQL connection = new ConnectMSQL();
+        Login login = new Login();
+    }
+}

ce8188b4a97d6dccc1935e67f2ad6990d1999e91
VitorH3 <92180456+VitorH3@users.noreply.github.com>
2023-11-22 21:06:42 -0300

    Add files via upload

--- PSC/src/main/java/TelaADM/ADM.java
+++ PSC/src/main/java/TelaADM/ADM.java
@@ -33,10 +33,6 @@ public class ADM extends javax.swing.JFrame {
         jTextField1 = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jComboBox1 = new javax.swing.JComboBox<>();
-        jLabel4 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jLabel5 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
         jComboBox2 = new javax.swing.JComboBox<>();
         jLabel7 = new javax.swing.JLabel();
@@ -45,79 +41,57 @@ public class ADM extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("CADASTRO DE USUÁRIOS");
-        setMinimumSize(new java.awt.Dimension(600, 650));
+        setMinimumSize(new java.awt.Dimension(450, 500));
         setResizable(false);
         getContentPane().setLayout(null);
 
         jLabel2.setBackground(new java.awt.Color(0, 0, 0));
         jLabel2.setText("NOME");
         getContentPane().add(jLabel2);
-        jLabel2.setBounds(130, 70, 70, 30);
+        jLabel2.setBounds(70, 70, 70, 30);
 
         jScrollPane1.setForeground(new java.awt.Color(255, 255, 255));
         jScrollPane1.setViewportView(jTextPane1);
 
         getContentPane().add(jScrollPane1);
-        jScrollPane1.setBounds(130, 100, 330, 30);
+        jScrollPane1.setBounds(70, 100, 330, 30);
 
         jLabel1.setText("IDADE");
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(130, 140, 33, 16);
+        jLabel1.setBounds(70, 150, 33, 16);
         getContentPane().add(jTextField1);
-        jTextField1.setBounds(130, 160, 60, 30);
+        jTextField1.setBounds(70, 170, 60, 30);
 
         jLabel3.setText("SEXO");
         getContentPane().add(jLabel3);
-        jLabel3.setBounds(290, 140, 28, 16);
+        jLabel3.setBounds(260, 150, 28, 16);
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione", "Masculino", "Feminino", "Prefiro não informar" }));
         getContentPane().add(jComboBox1);
-        jComboBox1.setBounds(290, 160, 72, 30);
-
-        jLabel4.setText("LIVRO PREFERIDO 1");
-        getContentPane().add(jLabel4);
-        jLabel4.setBounds(130, 210, 110, 20);
-        getContentPane().add(jTextField2);
-        jTextField2.setBounds(130, 230, 330, 30);
-
-        jLabel5.setText("LIVRO PREFERIDO 2");
-        getContentPane().add(jLabel5);
-        jLabel5.setBounds(130, 340, 110, 20);
-
-        jTextField3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField3ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jTextField3);
-        jTextField3.setBounds(130, 360, 330, 30);
+        jComboBox1.setBounds(260, 170, 120, 30);
 
         jLabel6.setText("GÊNERO LIVRO 1");
         getContentPane().add(jLabel6);
-        jLabel6.setBounds(130, 270, 110, 16);
+        jLabel6.setBounds(70, 230, 110, 16);
 
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione", "Romance", "Ficção", "Técnico" }));
         getContentPane().add(jComboBox2);
-        jComboBox2.setBounds(130, 290, 72, 22);
+        jComboBox2.setBounds(70, 250, 100, 22);
 
         jLabel7.setText("GÊNERO LIVRO 2");
         getContentPane().add(jLabel7);
-        jLabel7.setBounds(130, 410, 100, 16);
+        jLabel7.setBounds(70, 300, 100, 16);
 
-        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione", "Romance", "Ficção", "Técnico" }));
         getContentPane().add(jComboBox3);
-        jComboBox3.setBounds(130, 430, 72, 22);
+        jComboBox3.setBounds(70, 320, 110, 22);
 
         jButton1.setText("CADASTRAR USUÁRIO");
         getContentPane().add(jButton1);
-        jButton1.setBounds(220, 490, 170, 40);
+        jButton1.setBounds(160, 370, 170, 40);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -162,14 +136,10 @@ public class ADM extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
     private javax.swing.JTextPane jTextPane1;
     // End of variables declaration//GEN-END:variables
 }
